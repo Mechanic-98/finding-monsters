c9c3b02430481d08b445669bc27d32f4c22165a4
Mechanic-98 <parasherabhi98@gmail.com>
2022-07-21 01:44:37 +0530

    setState implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Abhishek',
+		};
+	}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Hi {this.state.name}</p>
+					<button
+						onClick={() => {
+							this.setState(
+								() => {
+									// shallow merge
+									return { name: 'Ojus' };
+								},
+								() => {
+									console.log(this.state.name);
+								}
+							);
+						}}
+					>
+						Change name
+					</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
